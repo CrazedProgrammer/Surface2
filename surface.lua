c45ec8ba360380b935afad9b7b6b7773c102a797
CrazedProgrammer <crazedprogrammer@gmail.com>
2017-01-15 23:24:20 +0100

    Added BMP support and changed some wrong variable names

--- surface.lua
+++ surface.lua
@@ -42,18 +42,18 @@ end
 
 
 local function calcStack(stack, width, height)
-	local sx, sy, cx, cy, cwidth, cheight = 0, 0, 0, 0, width, height
+	local ox, oy, cx, cy, cwidth, cheight = 0, 0, 0, 0, width, height
 	for i = 1, #stack do
 		if stack[i].shift then
-			sx = sx + stack[i].x
-			sy = sy + stack[i].y
+			ox = ox + stack[i].x
+			oy = oy + stack[i].y
 		end
 		cx = cx + stack[i].x
 		cy = cy + stack[i].y
 		cwidth = stack[i].width
 		cheight = stack[i].height
 	end
-	return sx, sy, cx, cy, cwidth, cheight
+	return ox, oy, cx, cy, cwidth, cheight
 end
 
 local function clipRect(x, y, width, height, cx, cy, cwidth, cheight)
@@ -83,7 +83,7 @@ function surface.create(width, height, b, t, c)
 	surface.buffer = { }
 	surface.overwrite = false
 	surface.stack = { }
-	surface.sx, surface.sy, surface.cx, surface.cy, surface.cwidth, surface.cheight = calcStack(surface.stack, width, height)
+	surface.ox, surface.oy, surface.cx, surface.cy, surface.cwidth, surface.cheight = calcStack(surface.stack, width, height)
 	
 	-- force array indeces instead of hashed indeces
 	local buffer = surface.buffer
@@ -151,6 +151,23 @@ function surface.load(strpath, isstr)
 
 	elseif data:sub(1, 2) == "BM" then
 		-- BMP format
+		local width = data:byte(0x13) + data:byte(0x14) * 256
+		local height = data:byte(0x17) + data:byte(0x18) * 256
+		if data:byte(0xF) ~= 0x28 or data:byte(0x1B) ~= 1 or data:byte(0x1D) ~= 0x18 then
+			error("unsupported bmp format, only uncompressed 24-bit rgb is supported.")
+		end
+		local offset, linesize = 0x36, math.ceil((width * 3) / 4) * 4
+		
+		local surf = surface.create(width, height)
+		local buffer = surf.buffer
+		for j = 0, height - 1 do
+			for i = 0, width - 1 do
+				buffer[(j * width + i) * 3 + 1] = data:byte((height - j - 1) * linesize + i * 3 + offset + 3) / 255
+				buffer[(j * width + i) * 3 + 2] = data:byte((height - j - 1) * linesize + i * 3 + offset + 2) / 255
+				buffer[(j * width + i) * 3 + 3] = data:byte((height - j - 1) * linesize + i * 3 + offset + 1) / 255
+			end
+		end
+		return surf
 
 	elseif data:find("\30") then
 		-- NFT format
@@ -229,29 +246,28 @@ end
 
 
 
-function surf:output(output, x, y, ox, oy, owidth, oheight)
+function surf:output(output, x, y, sx, sy, swidth, sheight)
 	output = output or (term or gpu)
 	if love then output = output or love.graphics end
 	x = x or 0
 	y = y or 0
-	ox = ox or 0
-	oy = oy or 0
-	owidth = owidth or self.width
-	oheight = oheight or self.height
-	ox, oy, owidth, oheight = clipRect(ox, oy, owidth, oheight, 0, 0, self.width, self.height)
+	sx = sx or 0
+	sy = sy or 0
+	swidth = swidth or self.width
+	sheight = sheight or self.height
+	sx, sy, swidth, sheight = clipRect(sx, sy, swidth, sheight, 0, 0, self.width, self.height)
 	
 	local buffer = self.buffer
-	local swidth = self.width
-	local sheight = self.height
+	local bwidth = self.width
 
 	if output.blit and output.setCursorPos then
 		-- CC
 		local dblit, dsetpos, cmd, str, text, back = output.blit, output.setCursorPos, { }, { }, { }, { }
-		for j = 0, oheight - 1 do
-			for i = 0, owidth - 1 do
-				str[i + 1] = buffer[((j + oy) * swidth + (i + ox)) * 3 + 2] or " "
-				text[i + 1] = _cc_color_to_hex[buffer[((j + oy) * swidth + (i + ox)) * 3 + 3] or 1]
-				back[i + 1] = _cc_color_to_hex[buffer[((j + oy) * swidth + (i + ox)) * 3 + 1]or 32768]
+		for j = 0, sheight - 1 do
+			for i = 0, swidth - 1 do
+				str[i + 1] = buffer[((j + sy) * bwidth + (i + sx)) * 3 + 2] or " "
+				text[i + 1] = _cc_color_to_hex[buffer[((j + sy) * bwidth + (i + sx)) * 3 + 3] or 1]
+				back[i + 1] = _cc_color_to_hex[buffer[((j + sy) * bwidth + (i + sx)) * 3 + 1]or 32768]
 			end
 			dsetpos(x + 1, y + j + 1)
 			dblit(table_concat(str), table_concat(text), table_concat(back))
@@ -263,21 +279,21 @@ function surf:output(output, x, y, ox, oy, owidth, oheight)
 	elseif output.blitPixels then
 		-- Riko 4
 		local pixels = { }
-		for j = 0, oheight - 1 do
-			for i = 0, owidth - 1 do
-				pixels[j * owidth + i + 1] = buffer[((j + oy) * swidth + (i + ox)) * 3 + 1] or 0
+		for j = 0, sheight - 1 do
+			for i = 0, swidth - 1 do
+				pixels[j * swidth + i + 1] = buffer[((j + sy) * bwidth + (i + sx)) * 3 + 1] or 0
 			end
 		end
-		output.blitPixels(x, y, owidth, oheight, pixels)
+		output.blitPixels(x, y, swidth, sheight, pixels)
 	
 	elseif output.points and output.setColor then
 		-- Love2D
 		local pos, r, g, b, pr, pg, pb = { }
-		for j = 0, oheight - 1 do
-			for i = 0, owidth - 1 do
-				pr = buffer[((j + oy) * swidth + (i + ox)) * 3 + 1]
-				pg = buffer[((j + oy) * swidth + (i + ox)) * 3 + 2]
-				pb = buffer[((j + oy) * swidth + (i + ox)) * 3 + 3]
+		for j = 0, sheight - 1 do
+			for i = 0, swidth - 1 do
+				pr = buffer[((j + sy) * bwidth + (i + sx)) * 3 + 1]
+				pg = buffer[((j + sy) * bwidth + (i + sx)) * 3 + 2]
+				pb = buffer[((j + sy) * bwidth + (i + sx)) * 3 + 3]
 				if pr ~= r or pg ~= g or pb ~= b then 
 					if #pos ~= 0 then
 						output.setColor((r or 0) * 255, (g or 0) * 255, (b or 0) * 255, (r or g or b) and 255 or 0)
@@ -297,17 +313,15 @@ function surf:output(output, x, y, ox, oy, owidth, oheight)
 end
 
 function surf:drawString(x, y, str, b, t)
-	local ox = x
+	x, y = x + self.ox, y + self.oy
+	local sx = x
 	for i = 1, #str do
 		local c = str:sub(i, i)
 		if c == "\n" then
-			x = ox
+			x = sx
 			y = y + 1
 		else
-			if x >= self.cx 
-				and x < self.cx + self.cwidth 
-				and y >= self.cy 
-				and y < self.cy + self.cheight then
+			if x >= self.cx  and x < self.cx + self.cwidth and y >= self.cy and y < self.cy + self.cheight then
 				self.buffer[(y * self.width + x) * 3 + 2] = c
 				self.buffer[(y * self.width + x) * 3 + 1] = b
 				self.buffer[(y * self.width + x) * 3 + 3] = t
