7634d55ac2b20eb4fafbb520e7cf52e2004e84a2
CrazedProgrammer <crazedprogrammer@gmail.com>
2017-01-15 01:07:57 +0100

    Added Riko-8 and Love2D outputs Added RIF, NFT and NFP image loading

--- surface.lua
+++ surface.lua
@@ -27,6 +27,20 @@ do
 local surf = { }
 surface.surf = surf
 
+local table_concat, math_floor = table.concat, math.floor
+
+local _cc_color_to_hex, _cc_hex_to_color = { }, { }
+for i = 0, 15 do
+	_cc_color_to_hex[2 ^ i] = string.format("%01x", i)
+	_cc_hex_to_color[string.format("%01x", i)] = 2 ^ i
+end
+
+local _chars = { }
+for i = 0, 255 do
+	_chars[i] = string.char(i)
+end
+
+
 local function calcStack(stack, width, height)
 	local sx, sy, cx, cy, cwidth, cheight = 0, 0, 0, 0, width, height
 	for i = 1, #stack do
@@ -69,7 +83,7 @@ function surface.create(width, height, b, t, c)
 	surface.buffer = { }
 	surface.overwrite = false
 	surface.stack = { }
-	surface.sx, surface.sy, surface.cx, surface.cy, surface.cwidth, surface.cheight = calcStack(surface.stack)
+	surface.sx, surface.sy, surface.cx, surface.cy, surface.cwidth, surface.cheight = calcStack(surface.stack, width, height)
 	
 	-- force array indeces instead of hashed indeces
 	local buffer = surface.buffer
@@ -98,9 +112,126 @@ function surface.create(width, height, b, t, c)
 	return surface
 end
 
-function surf:output(display, x, y, ox, oy, owidth, oheight)
-	display = display or (term or gpu)
-	if love then display = display or love.graphics end
+function surface.load(strpath, isstr)
+	local data = strpath
+	if not isstr then
+		local handle = io.open(strpath, "rb")
+		if not handle then return end
+		chars = { }
+		local byte = handle:read(1)
+		while byte do
+			chars[#chars + 1] = _chars[byte]
+			byte = handle:read(1)
+		end
+		handle:close()
+		data = table_concat(chars)
+	end
+	
+	if data:sub(1, 3) == "RIF" then
+		-- Riko 4 image format
+		local width, height = data:byte(4) * 256 + data:byte(5), data:byte(6) * 256 + data:byte(7)
+		local surf = surface.create(width, height)
+		local buffer = surf.buffer
+		local upper, byte = 8, false
+		local byte = data:byte(index)
+
+		for j = 0, height - 1 do
+			for i = 0, height - 1 do
+				if not upper then
+					buffer[(j * width + i) * 3 + 1] = math_floor(byte / 16)
+				else
+					buffer[(j * width + i) * 3 + 1] = byte % 16
+					index = index + 1
+					data = data:byte(index)
+				end
+				upper = not upper
+			end
+		end
+		return surf
+
+	elseif data:sub(1, 2) == "BM" then
+		-- BMP format
+
+	elseif data:find("\30") then
+		-- NFT format
+		local width, height, lwidth = 0, 1, 0
+		for i = 1, #data do
+			if data:byte(i) == 10 then -- newline
+				height = height + 1
+				if lwidth > width then
+					width = lwidth
+				end
+				lwidth = 0
+			elseif data:byte(i) == 30 or data:byte(i) == 31 then -- color control
+				lwidth = lwidth - 1
+			elseif data:byte(i) ~= 13 then -- not carriage return
+				lwidth = lwidth + 1
+			end
+		end
+
+		local surf = surface.create(width, height)
+		local buffer = surf.buffer
+		local index, x, y, b, t = 1, 0, 0
+
+		while index <= #data do
+			if data:byte(index) == 10 then
+				x, y = 0, y + 1
+			elseif data:byte(index) == 30 then
+				index = index + 1
+				b = _cc_hex_to_color[data:sub(index, index)]
+			elseif data:byte(index) == 31 then
+				index = index + 1
+				t = _cc_hex_to_color[data:sub(index, index)]
+			elseif data:byte(index) ~= 13 then
+				buffer[(y * width + x) * 3 + 1] = b
+				if b or t then
+					buffer[(y * width + x) * 3 + 2] = data:sub(index, index)
+				elseif data:sub(index, index) ~= " " then
+					buffer[(y * width + x) * 3 + 2] = data:sub(index, index)
+				end
+				buffer[(y * width + x) * 3 + 3] = t
+				x = x + 1
+			end
+			index = index + 1
+		end
+
+		return surf
+	else
+		-- NFP format
+		local width, height, lwidth = 0, 1, 0
+		for i = 1, #data do
+			if data:byte(i) == 10 then -- newline
+				height = height + 1
+				if lwidth > width then
+					width = lwidth
+				end
+				lwidth = 0
+			elseif data:byte(i) ~= 13 then -- not carriage return
+				lwidth = lwidth + 1
+			end
+		end
+
+		local surf = surface.create(width, height)
+		local buffer = surf.buffer
+		local x, y = 0, 0
+		for i = 1, #data do
+			if data:byte(i) == 10 then
+				x, y = 0, y + 1
+			elseif data:byte(i) ~= 13 then
+				buffer[(y * width + x) * 3 + 1] = _cc_hex_to_color[data:sub(i, i)]
+				x = x + 1
+			end
+		end
+
+		return surf
+	end
+end
+
+
+
+function surf:output(output, x, y, ox, oy, owidth, oheight)
+	output = output or (term or gpu)
+	if love then output = output or love.graphics end
 	x = x or 0
 	y = y or 0
 	ox = ox or 0
@@ -110,22 +241,79 @@ function surf:output(display, x, y, ox, oy, owidth, oheight)
 	ox, oy, owidth, oheight = clipRect(ox, oy, owidth, oheight, 0, 0, self.width, self.height)
 	
 	local buffer = self.buffer
+	local swidth = self.width
+	local sheight = self.height
 
-	if display.blit and display.setCursorPos then
+	if output.blit and output.setCursorPos then
 		-- CC
-		
+		local dblit, dsetpos, cmd, str, text, back = output.blit, output.setCursorPos, { }, { }, { }, { }
+		for j = 0, oheight - 1 do
+			for i = 0, owidth - 1 do
+				str[i + 1] = buffer[((j + oy) * swidth + (i + ox)) * 3 + 2] or " "
+				text[i + 1] = _cc_color_to_hex[buffer[((j + oy) * swidth + (i + ox)) * 3 + 3] or 1]
+				back[i + 1] = _cc_color_to_hex[buffer[((j + oy) * swidth + (i + ox)) * 3 + 1]or 32768]
+			end
+			dsetpos(x + 1, y + j + 1)
+			dblit(table_concat(str), table_concat(text), table_concat(back))
+		end
 
-	elseif display.write and display.setCursorPos and display.setTextColor and display.setBackgroundColor then
+	elseif output.write and output.setCursorPos and output.setTextColor and output.setBackgroundColor then
 		-- CC old
 	
-	elseif display.drawPixel then
+	elseif output.blitPixels then
 		-- Riko 4
+		local pixels = { }
+		for j = 0, oheight - 1 do
+			for i = 0, owidth - 1 do
+				pixels[j * owidth + i + 1] = buffer[((j + oy) * swidth + (i + ox)) * 3 + 1] or 0
+			end
+		end
+		output.blitPixels(x, y, owidth, oheight, pixels)
 	
-	elseif display.points and display.setColor then
+	elseif output.points and output.setColor then
 		-- Love2D
+		local pos, r, g, b, pr, pg, pb = { }
+		for j = 0, oheight - 1 do
+			for i = 0, owidth - 1 do
+				pr = buffer[((j + oy) * swidth + (i + ox)) * 3 + 1]
+				pg = buffer[((j + oy) * swidth + (i + ox)) * 3 + 2]
+				pb = buffer[((j + oy) * swidth + (i + ox)) * 3 + 3]
+				if pr ~= r or pg ~= g or pb ~= b then 
+					if #pos ~= 0 then
+						output.setColor((r or 0) * 255, (g or 0) * 255, (b or 0) * 255, (r or g or b) and 255 or 0)
+						output.points(pos)
+					end
+					r, g, b = pr, pg, pb
+					pos = { }
+				end
+				pos[#pos + 1] = i + x
+				pos[#pos + 1] = j + y
+			end
+		end
 	
 	else
-		error("unsupported display object")
+		error("unsupported output object")
+	end
+end
+
+function surf:drawString(x, y, str, b, t)
+	local ox = x
+	for i = 1, #str do
+		local c = str:sub(i, i)
+		if c == "\n" then
+			x = ox
+			y = y + 1
+		else
+			if x >= self.cx 
+				and x < self.cx + self.cwidth 
+				and y >= self.cy 
+				and y < self.cy + self.cheight then
+				self.buffer[(y * self.width + x) * 3 + 2] = c
+				self.buffer[(y * self.width + x) * 3 + 1] = b
+				self.buffer[(y * self.width + x) * 3 + 3] = t
+			end
+			x = x + 1
+		end
 	end
 end
 
