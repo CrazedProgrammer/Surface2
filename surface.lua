354b518fb51c1e753def36e31f9ae218da9fadd1
CrazedProgrammer <crazedprogrammer@gmail.com>
2017-01-19 23:30:39 +0000

    Added surf:flip and surf:shift

--- surface.lua
+++ surface.lua
@@ -638,8 +638,8 @@ end
 
 function surf:drawSurfaceRotated(surf2, x, y, ox, oy, angle)
 	local sin, cos, sx, sy, px, py = math.sin(angle), math.cos(angle)
-	for j = -surf2.height, surf2.height do
-		for i = -surf2.width, surf2.width do
+	for j = math.floor(-surf2.height * 0.75), math.ceil(surf2.height * 0.75) do
+		for i = math.floor(-surf2.width * 0.75), math.ceil(surf2.width * 0.75) do
 			sx, sy, px, py = x + i, y + j, math_floor(cos * (i + 0.5) - sin * (j + 0.5) + ox), math_floor(sin * (i + 0.5) + cos * (j + 0.5) + oy)
 			if sx >= self.cx and sx < self.cx + self.cwidth and sy >= self.cy and sy < self.cy + self.cheight and px >= 0 and px < surf2.width and py >= 0 and py < surf2.height then
 				b = surf2.buffer[(py * surf2.width + px) * 3 + 1]
@@ -659,6 +659,62 @@ function surf:drawSurfaceRotated(surf2, x, y, ox, oy, angle)
 	end
 end
 
+function surf:flip(horizontal, vertical)
+	local ox, oy, nx, ny, tb, tt, tc
+	if horizontal then
+		for i = 0, math.ceil(self.cwidth / 2) - 1 do
+			for j = 0, self.cheight - 1 do
+				ox, oy, nx, ny = i + self.cx, j + self.cy, self.cx + self.cwidth - i - 1, j + self.cy
+				tb = self.buffer[(oy * self.width + ox) * 3 + 1]
+				tt = self.buffer[(oy * self.width + ox) * 3 + 2]
+				tc = self.buffer[(oy * self.width + ox) * 3 + 3]
+				self.buffer[(oy * self.width + ox) * 3 + 1] = self.buffer[(ny * self.width + nx) * 3 + 1]
+				self.buffer[(oy * self.width + ox) * 3 + 2] = self.buffer[(ny * self.width + nx) * 3 + 2]
+				self.buffer[(oy * self.width + ox) * 3 + 3] = self.buffer[(ny * self.width + nx) * 3 + 3]
+				self.buffer[(ny * self.width + nx) * 3 + 1] = tb
+				self.buffer[(ny * self.width + nx) * 3 + 2] = tt
+				self.buffer[(ny * self.width + nx) * 3 + 3] = tc
+			end
+		end
+	end
+	if vertical then
+		for j = 0, math.ceil(self.cheight / 2) - 1 do
+			for i = 0, self.cwidth - 1 do
+				ox, oy, nx, ny = i + self.cx, j + self.cy, i + self.cx, self.cy + self.cheight - j - 1
+				tb = self.buffer[(oy * self.width + ox) * 3 + 1]
+				tt = self.buffer[(oy * self.width + ox) * 3 + 2]
+				tc = self.buffer[(oy * self.width + ox) * 3 + 3]
+				self.buffer[(oy * self.width + ox) * 3 + 1] = self.buffer[(ny * self.width + nx) * 3 + 1]
+				self.buffer[(oy * self.width + ox) * 3 + 2] = self.buffer[(ny * self.width + nx) * 3 + 2]
+				self.buffer[(oy * self.width + ox) * 3 + 3] = self.buffer[(ny * self.width + nx) * 3 + 3]
+				self.buffer[(ny * self.width + nx) * 3 + 1] = tb
+				self.buffer[(ny * self.width + nx) * 3 + 2] = tt
+				self.buffer[(ny * self.width + nx) * 3 + 3] = tc
+			end
+		end
+	end
+end
+
+function surf:shift(x, y)
+	local hdir, vdir = x < 0, y < 0
+	local xstart, xend = self.cx, self.cx + self.cwidth - 1
+	local ystart, yend = self.cy, self.cy + self.cheight - 1
+	local nx, ny
+	for j = vdir and ystart or yend, vdir and yend or ystart, vdir and 1 or -1 do
+		for i = hdir and xstart or xend, hdir and xend or xstart, hdir and 1 or -1 do
+			nx, ny = i - x, j - y
+			if nx >= 0 and nx < self.width and ny >= 0 and ny < self.height then
+				self.buffer[(j * self.width + i) * 3 + 1] = self.buffer[(ny * self.width + nx) * 3 + 1]
+				self.buffer[(j * self.width + i) * 3 + 2] = self.buffer[(ny * self.width + nx) * 3 + 2]
+				self.buffer[(j * self.width + i) * 3 + 3] = self.buffer[(ny * self.width + nx) * 3 + 3] 
+			else
+				self.buffer[(j * self.width + i) * 3 + 1] = nil
+				self.buffer[(j * self.width + i) * 3 + 2] = nil
+				self.buffer[(j * self.width + i) * 3 + 3] = nil
+			end
+		end
+	end
+end
 
 
 end
