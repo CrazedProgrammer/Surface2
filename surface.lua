6c3302dc32b1bc02edd1fc33574e1f3b0be4a895
CrazedProgrammer <crazedprogrammer@gmail.com>
2017-01-19 17:41:57 +0000

    Added surf:drawSurface and surf:drawSurfaceRotated

--- surface.lua
+++ surface.lua
@@ -539,6 +539,126 @@ function surf:fillRect(x, y, width, height, b, t, c)
 	end
 end
 
+function surf:drawSurface(surf2, x, y, width, height, sx, sy, swidth, sheight)
+	x, y, width, height, sx, sy, swidth, sheight = x + self.ox, y + self.oy, width or surf2.width, height or surf2.height, sx or 0, sy or 0, swidth or surf2.width, sheight or surf2.height
+
+	if width == swidth and height == sheight then
+		local nx, ny
+		nx, ny, width, height = clipRect(x, y, width, height, self.cx, self.cy, self.cwidth, self.cheight)
+		swidth, sheight = width, height
+		if nx > x then
+			sx = sx + nx - x
+			x = nx
+		end
+		if ny > y then
+			sy = sy + ny - y
+			y = ny
+		end
+		nx, ny, swidth, sheight = clipRect(sx, sy, swidth, sheight, 0, 0, surf2.width, surf2.height)
+		width, height = swidth, sheight
+		if nx > sx then
+			x = x + nx - sx
+			sx = nx
+		end
+		if ny > sy then
+			y = y + ny - sy
+			sy = ny
+		end
+
+		local b, t, c
+		for j = 0, height - 1 do
+			for i = 0, width - 1 do
+				b = surf2.buffer[((j + sy) * surf2.width + i + sx) * 3 + 1]
+				t = surf2.buffer[((j + sy) * surf2.width + i + sx) * 3 + 2]
+				c = surf2.buffer[((j + sy) * surf2.width + i + sx) * 3 + 3]
+				if b or self.overwrite then
+					self.buffer[((j + y) * self.width + i + x) * 3 + 1] = b
+				end
+				if t or self.overwrite then
+					self.buffer[((j + y) * self.width + i + x) * 3 + 2] = t
+				end
+				if c or self.overwrite then
+					self.buffer[((j + y) * self.width + i + x) * 3 + 3] = c
+				end
+			end
+		end
+	else
+		local hmirror, vmirror = false, false
+		if width < 0 then
+			hmirror = true
+			x = x + width
+		end
+		if height < 0 then
+			vmirror = true
+			y = y + height
+		end
+		if swidth < 0 then
+			hmirror = not hmirror
+			sx = sx + swidth
+		end
+		if sheight < 0 then
+			vmirror = not vmirror
+			sy = sy + sheight
+		end
+		width, height, swidth, sheight = math.abs(width), math.abs(height), math.abs(swidth), math.abs(sheight)
+		
+		local xscale, yscale, px, py, ssx, ssy, b, t, c = swidth / width, sheight / height
+		for j = 0, height - 1 do
+			for i = 0, width - 1 do
+				px, py = math_floor((i + 0.5) * xscale), math_floor((j + 0.5) * yscale) 
+				if hmirror then
+					ssx = x + width - i - 1
+				else
+					ssx = i + x
+				end
+				if vmirror then
+					ssy = y + height - j - 1
+				else
+					ssy = j + y
+				end
+
+				if ssx >= self.cx and ssx < self.cx + self.cwidth and ssy >= self.cy and ssy < self.cy + self.cheight and px >= 0 and px < surf2.width and py >= 0 and py < surf2.height then
+					b = surf2.buffer[(py * surf2.width + px) * 3 + 1]
+					t = surf2.buffer[(py * surf2.width + px) * 3 + 2]
+					c = surf2.buffer[(py * surf2.width + px) * 3 + 3]
+					if b or self.overwrite then
+						self.buffer[(ssy * self.width + ssx) * 3 + 1] = b
+					end
+					if t or self.overwrite then
+						self.buffer[(ssy * self.width + ssx) * 3 + 2] = t
+					end
+					if c or self.overwrite then
+						self.buffer[(ssy * self.width + ssx) * 3 + 3] = c
+					end
+				end
+			end
+		end
+	end
+end
+
+function surf:drawSurfaceRotated(surf2, x, y, ox, oy, angle)
+	local sin, cos, sx, sy, px, py = math.sin(angle), math.cos(angle)
+	for j = -surf2.height, surf2.height do
+		for i = -surf2.width, surf2.width do
+			sx, sy, px, py = x + i, y + j, math_floor(cos * (i + 0.5) - sin * (j + 0.5) + ox), math_floor(sin * (i + 0.5) + cos * (j + 0.5) + oy)
+			if sx >= self.cx and sx < self.cx + self.cwidth and sy >= self.cy and sy < self.cy + self.cheight and px >= 0 and px < surf2.width and py >= 0 and py < surf2.height then
+				b = surf2.buffer[(py * surf2.width + px) * 3 + 1]
+				t = surf2.buffer[(py * surf2.width + px) * 3 + 2]
+				c = surf2.buffer[(py * surf2.width + px) * 3 + 3]
+				if b or self.overwrite then
+					self.buffer[(sy * self.width + sx) * 3 + 1] = b
+				end
+				if t or self.overwrite then
+					self.buffer[(sy * self.width + sx) * 3 + 2] = t
+				end
+				if c or self.overwrite then
+					self.buffer[(sy * self.width + sx) * 3 + 3] = c
+				end
+			end
+		end
+	end
+end
+
 
 
 end
