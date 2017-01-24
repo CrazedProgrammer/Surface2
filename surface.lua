976cbaee73603c3e31710bab5a756501296d26c8
CrazedProgrammer <crazedprogrammer@gmail.com>
2017-01-24 00:06:30 +0000

    Added surf:map, surf:toRGB and surf:toPalette

--- surface.lua
+++ surface.lua
@@ -40,6 +40,48 @@ for i = 0, 255 do
 	_chars[i] = string.char(i)
 end
 
+local _steps, _palette, _rgbpal, _palr, _palg, _palb = 16
+
+local function setPalette(palette)
+	if palette == _palette then return end
+	_palette = palette
+	_rgbpal, _palr, _palg, _palb = { }, { }, { }, { }
+
+	local indices = { }
+	for k, v in pairs(_palette) do
+		if type(v) == "string" then
+			_palr[k] = tonumber(v:sub(1, 2), 16) / 255
+			_palg[k] = tonumber(v:sub(3, 4), 16) / 255
+			_palb[k] = tonumber(v:sub(5, 6), 16) / 255
+		elseif type(v) == "number" then
+			_palr[k] = math.floor(v / 65536) / 255
+			_palg[k] = (math.floor(v / 256) % 256) / 255
+			_palb[k] = (v % 256) / 255
+		end
+		indices[#indices + 1] = k
+	end
+
+	local pr, pg, pb, dist, d, id
+	for i = 0, _steps - 1 do
+		for j = 0, _steps - 1 do
+			for k = 0, _steps - 1 do
+				pr = (i + 0.5) / _steps
+				pg = (j + 0.5) / _steps
+				pb = (k + 0.5) / _steps
+
+				dist = 1e10
+				for l = 1, #indices do
+					d = (pr - _palr[indices[l]]) ^ 2 + (pg - _palg[indices[l]]) ^ 2 + (pb - _palb[indices[l]]) ^ 2
+					if d < dist then
+						dist = d
+						id = l
+					end
+				end
+				_rgbpal[i * _steps * _steps + j * _steps + k + 1] = indices[id]
+			end
+		end
+	end
+end
 
 local function calcStack(stack, width, height)
 	local ox, oy, cx, cy, cwidth, cheight = 0, 0, 0, 0, width, height
@@ -73,6 +115,9 @@ local function clipRect(x, y, width, height, cx, cy, cwidth, cheight)
 end
 
 
+
+surface.palette = { }
+surface.palette.cc = {[1]="F0F0F0",[2]="F2B233",[4]="E57FD8",[8]="99B2F2",[16]="DEDE6C",[32]="7FCC19",[64]="F2B2CC",[128]="4C4C4C",[256]="999999",[512]="4C99B2",[1024]="B266E5",[2048]="3366CC",[4096]="7F664C",[8192]="57A64E",[16384]="CC4C4C",[32768]="191919"}
 
 function surface.create(width, height, b, t, c)
 	local surface = setmetatable({ }, {__index = surface.surf})
@@ -849,6 +894,49 @@ function surf:shift(x, y)
 				self.buffer[(j * self.width + i) * 3 + 2] = nil
 				self.buffer[(j * self.width + i) * 3 + 3] = nil
 			end
+		end
+	end
+end
+
+function surf:map(colors)
+	local c
+	for j = self.cy, self.cy + self.cheight - 1 do
+		for i = self.cx, self.cx + self.cwidth - 1 do
+			c = colors[self.buffer[(j * self.width + i) * 3 + 1]]
+			if c or self.overwrite then
+				self.buffer[(j * self.width + i) * 3 + 1] = c
+			end
+		end
+	end
+end
+
+function surf:toRGB(palette)
+	setPalette(palette)
+	local c
+	for j = 0, self.height - 1 do
+		for i = 0, self.width - 1 do
+			c = self.buffer[(j * self.width + i) * 3 + 1] 
+			self.buffer[(j * self.width + i) * 3 + 1] = _palr[c]
+			self.buffer[(j * self.width + i) * 3 + 2] = _palg[c]
+			self.buffer[(j * self.width + i) * 3 + 3] = _palb[c]
+		end
+	end
+end
+
+function surf:toPalette(palette, dither) -- todo: dithering
+	setPalette(palette)
+	local scale, r, g, b, nr, ng, nb = _steps - 1
+	for j = 0, self.height - 1 do
+		for i = 0, self.width - 1 do
+			r = self.buffer[(j * self.width + i) * 3 + 1]
+			g = self.buffer[(j * self.width + i) * 3 + 2]
+			b = self.buffer[(j * self.width + i) * 3 + 3]
+			nr = math_floor(r * scale + 0.5)
+			ng = math_floor(g * scale + 0.5)
+			nb = math_floor(b * scale + 0.5)
+			self.buffer[(j * self.width + i) * 3 + 1] = _rgbpal[nr * _steps * _steps + ng * _steps + nb + 1]
+			self.buffer[(j * self.width + i) * 3 + 2] = nil
+			self.buffer[(j * self.width + i) * 3 + 3] = nil
 		end
 	end
 end
