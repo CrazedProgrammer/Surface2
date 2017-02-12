52dcdb9243a68212500e94b9db17391839b018fb
CrazedProgrammer <crazedprogrammer@gmail.com>
2017-02-12 15:33:55 +0000

    Add CC hello world example and change font colour to white

---
+++ examples/cc/1-helloworld.lua
@@ -0,0 +1,15 @@
+-- Loads Surface 2.
+-- Change this path to wherever the surface 2 file is.
+local surface = dofile("Surface2/target/surface") 
+
+-- Creates a new surface with a blue background with a size of 51x19 pixels.
+local surf = surface.create(51, 19, colors.blue)
+
+-- Draws the string "Hello, world!" at position (2,2).
+surf:drawString("Hello, world!", 2, 2)
+
+-- Outputs the surface to the screen.
+surf:output()
+
+-- Waits for a mouse click.
+os.pullEvent("mouse_click")
