44b2fbdd81e599bf553c076a05eca1d9738bc25b
CrazedProgrammer <crazedprogrammer@gmail.com>
2017-02-12 15:00:23 +0000

    Small consistency fix

--- examples/general/imageconvert.lua
+++ examples/general/imageconvert.lua
@@ -1,5 +1,5 @@
 -- Loads Surface 2.
--- Change this path to wherever the surface 2 file is.
+-- Change this path to wherever the Surface 2 file is.
 local surface = dofile("Surface2/target/surface") 
 
 -- Loads the font.nfp image.
@@ -8,5 +8,5 @@ local surf = surface.load("Surface2/examples/font.nfp")
 -- Converts the surface to RGB.
 surf:toRGB(surface.palette.cc)
 
--- Saves the surface to font.bmp
+-- Saves the surface to font.bmp.
 surf:save("Surface2/examples/font.bmp", "bmp")
