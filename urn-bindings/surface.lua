003c3c1a73a2f174deb34857ea55acf2a295625e
CrazedProgrammer <crazedprogrammer@gmail.com>
2017-04-23 16:17:38 +0200

    Add bindings for Urn

---
+++ urn-bindings/surface.lua
@@ -0,0 +1,43 @@
+local surface = dofile("surface") or {}
+return {
+	["create"] = surface.create,
+	["get-text-size"] = surface.getTextSize,
+	["load!"] = surface.load,
+	["load-font"] = surface.loadFont,
+	["load-sprite-map"] = surface.loadSpriteMap,
+
+	["palette/cc"] = surface.palette and surface.palette.cc,
+	["palette/riko4"] = surface.palette and surface.palette.riko4,
+	["palette/redirection"] = surface.palette and surface.palette.redirection,
+
+	["surf/clear!"] = surface.surf.clear,
+	["surf/copy"] = surface.surf.copy,
+	["surf/draw-arc!"] = surface.surf.drawArc,
+	["surf/draw-ellipse!"] = surface.surf.drawEllipse,
+	["surf/draw-line!"] = surface.surf.drawLine,
+	["surf/draw-pixel!"] = surface.surf.drawPixel,
+	["surf/draw-rect!"] = surface.surf.drawRect,
+	["surf/draw-string!"] = surface.surf.drawString,
+	["surf/draw-surface!"] = surface.surf.drawSurface,
+	["surf/draw-surface-rotated!"] = surface.surf.drawSurfaceRotated,
+	["surf/draw-surfaces-interlaced!"] = surface.surf.drawSurfacesInterlaced,
+	["surf/draw-surface-small!"] = surface.surf.drawSurfaceSmall,
+	["surf/draw-text!"] = surface.surf.drawText,
+	["surf/draw-triangle!"] = surface.surf.drawTriangle,
+	["surf/fill-arc!"] = surface.surf.fillArc,
+	["surf/fill-ellipse!"] = surface.surf.fillEllipse,
+	["surf/fill-rect!"] = surface.surf.fillRect,
+	["surf/fill-triangle!"] = surface.surf.fillTriangle,
+	["surf/flip!"] = surface.surf.flip,
+	["surf/map!"] = surface.surf.map,
+	["surf/output!"] = surface.surf.output,
+	["surf/pop!"] = surface.surf.pop,
+	["surf/push!"] = surface.surf.push,
+	["surf/save!"] = surface.surf.save,
+	["surf/shift!"] = surface.surf.shift,
+	["surf/to-palette!"] = surface.surf.toPalette,
+	["surf/to-rgb!"] = surface.surf.toRGB,
+
+	["smap/pos"] = surface.smap.pos,
+	["smap/sprite"] = surface.smap.sprite
+}
