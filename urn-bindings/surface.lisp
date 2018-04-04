9be83569500167f7011a072d0ff4028f61b1e141
CrazedProgrammer <crazedprogrammer@gmail.com>
2018-04-04 22:10:34 +0200

    Use macro magic to generate bindings

--- urn-bindings/surface.lisp
+++ urn-bindings/surface.lisp
@@ -1,42 +1,52 @@
-(define-native load-lib!)
+(import lua/basic (_G dofile))
 
-(define-native create)
-(define-native get-text-size)
-(define-native load!)
-(define-native load-font)
-(define-native load-sprite-map)
+(define surface-bindings :hidden
+  { :create "surface.create"
+    :get-text-size "surface.getTextSize"
+    :load! "surface.load"
+    :load-font "surface.loadFont"
+    :load-sprite-map "surface.loadSpriteMap"
 
-(define-native palette/cc)
-(define-native palette/riko4)
-(define-native palette/redirection)
+    :palette/cc "surface.palette.cc"
+    :palette/riko4 "surface.palette.riko4"
+    :palette/redirection "surface.palette.redirection"
 
-(define-native surf/clear!)
-(define-native surf/copy)
-(define-native surf/draw-arc!)
-(define-native surf/draw-ellipse!)
-(define-native surf/draw-line!)
-(define-native surf/draw-pixel!)
-(define-native surf/draw-rect!)
-(define-native surf/draw-string!)
-(define-native surf/draw-surface!)
-(define-native surf/draw-surface-rotated!)
-(define-native surf/draw-surfaces-interlaced!)
-(define-native surf/draw-surface-small!)
-(define-native surf/draw-text!)
-(define-native surf/draw-triangle!)
-(define-native surf/fill-arc!)
-(define-native surf/fill-ellipse!)
-(define-native surf/fill-rect!)
-(define-native surf/fill-triangle!)
-(define-native surf/flip!)
-(define-native surf/map!)
-(define-native surf/output!)
-(define-native surf/pop!)
-(define-native surf/push!)
-(define-native surf/save!)
-(define-native surf/shift!)
-(define-native surf/to-palette!)
-(define-native surf/to-rgb!)
+    :surf/clear! "surface.surf.clear"
+    :surf/copy "surface.surf.copy"
+    :surf/draw-arc! "surface.surf.drawArc"
+    :surf/draw-ellipse! "surface.surf.drawEllipse"
+    :surf/draw-line! "surface.surf.drawLine"
+    :surf/draw-pixel! "surface.surf.drawPixel"
+    :surf/draw-rect! "surface.surf.drawRect"
+    :surf/draw-string! "surface.surf.drawString"
+    :surf/draw-surface! "surface.surf.drawSurface"
+    :surf/draw-surface-rotated! "surface.surf.drawSurfaceRotated"
+    :surf/draw-surfaces-interlaced! "surface.surf.drawSurfacesInterlaced"
+    :surf/draw-surface-small! "surface.surf.drawSurfaceSmall"
+    :surf/draw-text! "surface.surf.drawText"
+    :surf/draw-triangle! "surface.surf.drawTriangle"
+    :surf/fill-arc! "surface.surf.fillArc"
+    :surf/fill-ellipse! "surface.surf.fillEllipse"
+    :surf/fill-rect! "surface.surf.fillRect"
+    :surf/fill-triangle! "surface.surf.fillTriangle"
+    :surf/flip! "surface.surf.flip"
+    :surf/map! "surface.surf.map"
+    :surf/output! "surface.surf.output"
+    :surf/pop! "surface.surf.pop"
+    :surf/push! "surface.surf.push"
+    :surf/save! "surface.surf.save"
+    :surf/shift! "surface.surf.shift"
+    :surf/to-palette! "surface.surf.toPalette"
+    :surf/to-rgb! "surface.surf.toRGB"
 
-(define-native smap/pos)
-(define-native smap/sprite)
+    :smap/pos "surface.smap.pos"
+    :smap/sprite "surface.smap.sprite"})
+
+
+
+(defmacro load-surface! (path (lib-name 'surface))
+   @(cons `(define ,'surface-native-lib (dofile ,path))
+      (let* [(output '())]
+        (for-pairs (definition native) surface-bindings
+          (push! output `(define ,(string->symbol (.. (symbol->string lib-name) "/" definition)) (.> ,'surface-native-lib ,@(cdr (string/split native "%."))))))
+        output)))
